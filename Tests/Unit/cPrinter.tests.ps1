2925a80df720bd67c028c0184b6cf33ffdc73cca
Eric Boersma <boersma.eric.j@gmail.com>
2018-01-18 21:24:42 -0500

    Update cPrinter.tests.ps1

--- Tests/Unit/cPrinter.tests.ps1
+++ Tests/Unit/cPrinter.tests.ps1
@@ -4,7 +4,7 @@ $Global:DscResourceName = 'cPrinter'
 #region HEADER
 
 # Unit Test Template Version: 1.2.0
-<#$script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+$script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
 if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
      (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
 {
@@ -20,19 +20,12 @@ $TestEnvironment = Initialize-TestEnvironment `
 
 #endregion HEADER
 
-function Invoke-TestSetup {
-}
-
-function Invoke-TestCleanup {
-    Restore-TestEnvironment -TestEnvironment $TestEnvironment
-}
-
 # Begin Testing
 try {
     Invoke-TestSetup
-#>
-$script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
-Import-Module (Join-Path -Path $script:moduleRoot -ChildPath 'cPrinterManagement.psd1') -Force
+
+#$script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+#Import-Module (Join-Path -Path $script:moduleRoot -ChildPath 'cPrinterManagement.psd1') -Force
     #region Pester Tests
     InModuleScope -ModuleName cPrinter {
         Describe 'Test Method'{          
@@ -1485,9 +1478,9 @@ Import-Module (Join-Path -Path $script:moduleRoot -ChildPath 'cPrinterManagement
                 } # End It Update LPR/TCPIP Port SNMPIndex
             } # End Context Update Port Settings
         } # End Describe Set Method
-    } <#
+    } 
 } finally {
     #region FOOTER
     Restore-TestEnvironment -TestEnvironment $TestEnvironment
     #endregion
-}#>
+}
