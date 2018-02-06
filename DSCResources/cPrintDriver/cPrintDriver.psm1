3dad8b93c76674dd151739e18883c9ec00a727b8
Eric Boersma <boersma.eric.j@gmail.com>
2018-02-06 00:02:32 -0500

    Fixed syntax for PSSA rule

--- DSCResources/cPrintDriver/cPrintDriver.psm1
+++ DSCResources/cPrintDriver/cPrintDriver.psm1
@@ -22,28 +22,33 @@ class cPrintDriver {
     
     hidden $Messages = ""
     
-    cPrintDriver(){
+    cPrintDriver()
+    {
         $this.Messages = (Import-LocalizedData  -FileName 'cPrinterManagement.strings.psd1' -BaseDirectory (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $PSCOMMANDPATH))))
     }
-    [void] Set(){
+    [void] Set()
+    {
 
     } # End Set()
-    [bool] Test(){
+    [bool] Test()
+    {
         return $false
     } # End Test()
-    [cPrintDriver] Get(){
+    [cPrintDriver] Get()
+    {
         $ReturnObject = [cPrintDriver]::new()
-        Foreach ($name in $this.Name){
-            try 
+        Foreach ($name in $this.Name)
+        {
+            try
             {
-                $InstalledPrintDriver = Get-PrinterDriver -Name $this.Name -ErrorAction Stop
-            } 
-            catch 
+                $installedPrintDriver = Get-PrinterDriver -Name $this.Name -ErrorAction Stop
+            } # End Try
+            catch
             {
                 # Print driver isn't installed, need to look in the driver store to see if it is there. Only checking if the $Pruge is set to true
                 $ReturnObject.Ensure = [Ensure]::Absent
                 if($this.Purge -eq $true)
-                { 
+                {
                     $stagedDriver = $this.InstalledDriver()
                     if(-not [string]::IsNullOrEmpty($stagedDriver))
                     {
@@ -54,16 +59,17 @@ class cPrintDriver {
                 {
                     return $ReturnObject
                 } # End If ReturnObject.Enusre
-            
-            } # End try/catch Get-PrinterDriver
+            } # End catch      
         } # End Foreach Name
         return $ReturnObject
     } # End Get()
-    hidden [string] InstalledDriver() {
+    hidden [string] InstalledDriver() 
+    {
         # Since we don't have an INF file to look at. We need 
-        $InstalledDriverPacks = Get-WindowsDriver -Online -Verbose:$false | Where-Object {$_.ClassName -eq 'Printer' -and $_.Version -eq $this.Version}
-        foreach ($InstalledDriverPack in $InstalledDriverPacks) {
-            $DriverExists = Get-WindowsDriver -Online -Driver $InstalledDriverPack.Driver -Verbose:$false | Where-Object {$_.HardwareDescription -eq $this.Name}
+        $InstalledDriverPacks = Get-WindowsDriver -Online -All | Where-Object {$_.ClassName -eq 'Printer' -and $_.Version -eq $this.Version}
+        foreach ($InstalledDriverPack in $InstalledDriverPacks) 
+        {   
+            $DriverExists = Get-WindowsDriver -Online -Driver $InstalledDriverPack.Driver | Where-Object {$_.HardwareDescription -eq $this.Name}
             if($DriverExists)
             {
                 Write-Verbose "Found existing driver package at $($InstalledDriverPack.OriginalFileName)"
