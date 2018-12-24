a3e51e431d18f5c1c924a5df4add0761a9f97a90
Eric Boersma <limiteddenial@gmail.com>
2018-12-24 12:05:10 -0500

    Removed SNMPEnabled property and VScode formatted it

--- Tests/Integration/Printer.config.ps1
+++ Tests/Integration/Printer.config.ps1
@@ -1,25 +1,24 @@
 $TestPrinterLPR = [PSObject]@{
-    Ensure = 'Present'
-    Name = 'IntegrationLPR'
-    PortType = 'LPR'
-    PortName = 'IntegrationLPRPort'
-    Address = 'Test.local'
-    DriverName = 'Microsoft XPS Document Writer v4'
+    Ensure       = 'Present'
+    Name         = 'IntegrationLPR'
+    PortType     = 'LPR'
+    PortName     = 'IntegrationLPRPort'
+    Address      = 'Test.local'
+    DriverName   = 'Microsoft XPS Document Writer v4'
     LprQueueName = 'dummyQueue'
-    Shared = $false
+    Shared       = $false
 }
 
 $TestPrinterTCPIP = [PSObject]@{
-    Ensure = 'Present'
-    Name = 'IntegrationTCPIP'
-    PortType = 'TCPIP'
-    PortName = 'IntegrationTCPIPPort'
-    Address = 'Test.local'
-    DriverName = 'Microsoft XPS Document Writer v4'
-    Shared = $true
-    SNMPIndex = 1
-    SNMPCommunity = 'public'
-    SNMPEnabled = $true
+    Ensure         = 'Present'
+    Name           = 'IntegrationTCPIP'
+    PortType       = 'TCPIP'
+    PortName       = 'IntegrationTCPIPPort'
+    Address        = 'Test.local'
+    DriverName     = 'Microsoft XPS Document Writer v4'
+    Shared         = $true
+    SNMPIndex      = 1
+    SNMPCommunity  = 'public'
     PermissionSDDL = 'G:SYD:(A;OIIO;GA;;;CO)(A;OIIO;GA;;;AC)(A;;SWRC;;;WD)(A;CIIO;GX;;;WD)(A;;SWRC;;;AC)(A;CIIO;GX;;;AC)(A;;LCSWDTSDRCWDWO;;;BA)(A;OICIIO;GA;;;BA)'
 }
 
@@ -37,27 +36,26 @@ Configuration Printer_Config
     Node $NodeName
     {
         Printer LPRPrinter {
-            Ensure = $TestPrinterLPR.Ensure
-            Name = $TestPrinterLPR.Name
-            PortType = $TestPrinterLPR.PortType
-            PortName = $TestPrinterLPR.PortName
-            Address = $TestPrinterLPR.Address
-            DriverName = $TestPrinterLPR.DriverName
+            Ensure       = $TestPrinterLPR.Ensure
+            Name         = $TestPrinterLPR.Name
+            PortType     = $TestPrinterLPR.PortType
+            PortName     = $TestPrinterLPR.PortName
+            Address      = $TestPrinterLPR.Address
+            DriverName   = $TestPrinterLPR.DriverName
             LprQueueName = $TestPrinterLPR.LprQueueName
-            Shared = $TestPrinterLPR.Shared
+            Shared       = $TestPrinterLPR.Shared
         }
         
         Printer TCPIP-Printer {
-            Ensure = $TestPrinterTCPIP.Ensure
-            Name = $TestPrinterTCPIP.Name
-            PortType = $TestPrinterTCPIP.PortType
-            PortName = $TestPrinterTCPIP.PortName
-            Address = $TestPrinterTCPIP.Address
-            DriverName = $TestPrinterTCPIP.DriverName
-            Shared = $TestPrinterTCPIP.Shared
-            SNMPEnabled = $TestPrinterTCPIP.SNMPEnabled
-            SNMPCommunity = $TestPrinterTCPIP.SNMPCommunity
-            SNMPIndex = $TestPrinterTCPIP.SNMPIndex
+            Ensure         = $TestPrinterTCPIP.Ensure
+            Name           = $TestPrinterTCPIP.Name
+            PortType       = $TestPrinterTCPIP.PortType
+            PortName       = $TestPrinterTCPIP.PortName
+            Address        = $TestPrinterTCPIP.Address
+            DriverName     = $TestPrinterTCPIP.DriverName
+            Shared         = $TestPrinterTCPIP.Shared
+            SNMPCommunity  = $TestPrinterTCPIP.SNMPCommunity
+            SNMPIndex      = $TestPrinterTCPIP.SNMPIndex
             PermissionSDDL = $TestPrinterTCPIP.PermissionSDDL
         }
     }
